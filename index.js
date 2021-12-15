d2cc5cba4b46eb698bf9fa77933d37edac0b8394
Yakubov01 <lord17cl@gmail.com>
2021-12-15 22:06:33 +0500

    change port

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/audio.routes")(app);
 
 // set port, listen for requests
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
